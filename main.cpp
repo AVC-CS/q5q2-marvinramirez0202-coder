8e04cb14f503dee6c3cbe5edfb5d8fbec5e1fe5c
marvinramirez0202-coder <marvinramirez0202@gmail.com>
2026-04-13 13:57:59 -0700

    Updated main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main()
 {
     int N, M;
+    int result;
 
     cout << "Enter your base number: ";
     cin >> N;
@@ -13,7 +14,19 @@ int main()
     cout << N << " to power number: ";
 
     // TODO
-
+    result = 1;
+    cout << "1, ";
+    if(N > 0 & N < 10 & M >= 1 & M <= 10){
+        for(int i = 0; i < M; i++){
+            result = result * N;
+        if(i == M - 1){
+            cout << result << endl;
+        }
+        else{
+            cout << result << ", ";
+        }
+        }
+    }
     cout << endl;
 
     return 0;
